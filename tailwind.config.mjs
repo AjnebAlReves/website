8b55340b58111248497694e94e96dff263bcb72c
Ajneb Al Revés <terricraft3@outlook.com>
2026-01-10 02:04:57 +0000

    Update packages and tailwind hehe

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				dark: '#050505',
+				accent: '#ffcc00', // Ámbar Flyx
+				paraguay: '#d52b1e', // Un rojo sutil para detalles
+			},
+			fontFamily: {
+				sans: ['Inter', 'sans-serif'],
+				mono: ['JetBrains Mono', 'monospace'],
+			}
+		},
 	},
 	plugins: [],
 }
