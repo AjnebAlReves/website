c622f45b7bc66ddd015ef2d0f9735900592236da
Ajneb Al Revés <terricraft3@outlook.com>
2026-01-10 02:36:48 +0000

    Remove i18n implementation, keep Spanish hardcoded

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
+import path from 'path'
 
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://porfolio.dev/',
+
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve('./src')
+      }
+    }
+  }
 })
